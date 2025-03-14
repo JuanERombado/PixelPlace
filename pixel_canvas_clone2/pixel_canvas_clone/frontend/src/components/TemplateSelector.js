d7cddfd5103346c6248d249ead85c2c479d8400b
Juan Rombado <juanedrom@gmail.com>
2025-03-13 20:04:45 -0500

    Add files via upload

---
+++ pixel_canvas_clone2/pixel_canvas_clone/frontend/src/components/TemplateSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TemplateSelector = () => {
+  return (
+    <div className="template-selector">
+      <h3>Templates</h3>
+      <p>Template selection is under construction.</p>
+    </div>
+  );
+};
+
+export default TemplateSelector;
